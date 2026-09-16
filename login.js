ac64d3d9fc78c63f183a4a4572e579b2e4253679
tranminhduc4709-png <tranminhduc4709@gmail.com>
2026-09-16 16:15:40 +0700

    Create login.js

---
+++ login.js
@@ -0,0 +1,32 @@
+function checkLogin(username, password) {
+
+    if (username === "admin" && password === "123") {
+        return true;
+    }
+
+    return false;
+}
+
+
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const message = document.getElementById("message");
+
+    if (checkLogin(username, password)) {
+
+        message.innerText = "Đăng nhập thành công!";
+        message.style.color = "green";
+
+    } else {
+
+        message.innerText = "Sai tài khoản hoặc mật khẩu!";
+        message.style.color = "red";
+
+    }
+
+});
